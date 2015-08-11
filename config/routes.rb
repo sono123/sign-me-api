13aa4e9ac995db1e990714f2416ee4c0cd0f9bbd
steveono <steveono@gmail.com>
2015-08-11 00:25:43 -0400

    Create user routes with namespacing.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  
+  namespace :api do
+    namespace :v1 do
+      resources :users, :except => [:index]
+    end
+  end
+
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
