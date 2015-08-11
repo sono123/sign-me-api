9480d4a838d005ecbe4dc8b430ceda1f5acfc898
steveono <steveono@gmail.com>
2015-08-11 00:46:43 -0400

    Add associations for Users and Documents.

--- app/models/document.rb
+++ app/models/document.rb
@@ -1,2 +1,5 @@
 class Document < ActiveRecord::Base
+	belongs_to :user
+	has_attached_file :document, :styles => { :medium => "800x1100>", :thumb => "100x100>" },  :default_url => "/images/:style/missing.png"
+  # validates_attachment_content_type :document, :content_type => /\Aimage\/.*\Z/
 end
